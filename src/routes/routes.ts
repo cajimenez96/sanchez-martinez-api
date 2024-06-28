4de712cfcfd2987871c6c33a6d1d31f494164a3d
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-06-28 14:12:52 -0300

    Add routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import topicRouter from './topic.routes';
 import contactRouter from './contact.routes';
+import userRouter from './user.routes';
+// import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.use('/topic', topicRouter);
 router.use('/contact', contactRouter);
+router.use('/user', userRouter);
 
 export default router;
