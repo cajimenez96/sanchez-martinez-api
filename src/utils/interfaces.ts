689a44fc76002b21420d11e5d4fea7c8089e00ff
cajimenez96 <carlosj_utn@hotmail.com>
2024-06-29 12:23:11 -0300

    update interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,4 +1,15 @@
+import { Document } from 'mongoose';
 export interface IUser extends Document {
   userName: string;
   password: string;
+}
+
+export interface IPost extends Document {
+  title: string;
+  content: string;
+  images: string[];
+  author: string;
+  source: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
