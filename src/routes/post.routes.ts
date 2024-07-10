7cfec81700dfcb64fcbd0445ae905c942fae1469
cajimenez96 <carlosj_utn@hotmail.com>
2024-07-10 18:45:44 -0300

    Update routes

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -6,14 +6,16 @@ import {
   updatePostById,
   deletePostById
 } from '../controller/post.controller';
+import { authMiddleware } from '../middleware/authMiddleware';
+
 
 const topicRouter = express.Router();
 
 topicRouter.get('/', getAllPosts); //Obtener todos los posts
 topicRouter.get('/:id', getPostById); //Obtener un post
-topicRouter.post('/add', createPost); //Agregar un post
-topicRouter.patch('/update/:id', updatePostById); //modificar un post
-topicRouter.post('/delete/:id', deletePostById); //eliminar un post
+topicRouter.post('/add', /*authMiddleware,*/ createPost); //Agregar un post
+topicRouter.patch('/update/:id', authMiddleware, updatePostById); //modificar un post
+topicRouter.post('/delete/:id', authMiddleware, deletePostById); //eliminar un post
 
 
 export default topicRouter;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,11 +2,10 @@ import express from 'express';
 import postRouter from './post.routes';
 import contactRouter from './contact.routes';
 import userRouter from './user.routes';
-import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.use('/post', authMiddleware, postRouter);
+router.use('/post', postRouter);
 router.use('/contact', contactRouter);
 router.use('/user', userRouter);
 
